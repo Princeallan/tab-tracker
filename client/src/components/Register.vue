1bf1cb725f2ebc4898dc27291a707958db7bb76a
princeallan <allankirui57@gmail.com>
2018-11-22 17:42:06 +0300

    Register form styling

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,21 +1,37 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
-    type="text"
-    name="email"
-    v-model="email"
-    >
-    <br>
-    <input
-    type="password"
-    name="password"
-    v-model="password"
-    ><br>
-    <dir class="error" v-html="error"></dir>
-    <br>
-    <button @click="register">Register</button>
-  </div>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar color="cyan" dark>
+            <v-toolbar-title>Register form</v-toolbar-title>
+          </v-toolbar>
+
+          <v-card-text>
+          <v-text-field
+            type="text"
+            placeholder="Email"
+            name="email"
+            v-model="email"> </v-text-field>
+            <br>
+            <v-text-field
+            type="password"
+            placeholder="Password"
+            name="password"
+            v-model="password"> </v-text-field>
+            <br>
+            <div class="error" v-html="error"></div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="register" dark class="cyan">Register</v-btn>
+        </v-card-actions>
+
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -43,10 +59,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
   .error {
     color: red;
+    background: white !important;
   }
 
 </style>
