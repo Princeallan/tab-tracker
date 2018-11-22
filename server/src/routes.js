e6d98ec2bcae41d9be3514ab5a10ab790e89f9f0
princeallan <allankirui57@gmail.com>
2018-11-22 12:14:37 +0300

    AuthenticationController created

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,5 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+
 module.exports = (app) => {
-  app.post('/register', (req, res) => {
-    res.send({
-      message: `hello ${req.body.email}! user registered successfully, have fun!`
-    })
-  })
+  app.post('/register', AuthenticationController.register)
 }
