205a8d99d26877c0e6bd7c0edac70c77b3a1d08c
princeallan <allankirui57@gmail.com>
2018-12-19 22:44:38 +0300

    redirects and links added to header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,12 +1,43 @@
 <template>
-  <v-toolbar dark color="cyan">
+  <v-toolbar fixed color="cyan" dark>
+    <v-toolbar-title class="white--text mr-4">
+      <span class="home" @click="navigateTo({name: 'root'})">
+        Tab Tracker
+      </span>
+    </v-toolbar-title>
 
-    <v-toolbar-title class="white--text">Title</v-toolbar-title>
+    <v-toolbar-items>
+      <v-btn flat dark>Browse</v-btn>
+    </v-toolbar-items>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+        <v-btn flat dark @click="navigateTo({name: 'register'})">
+          SIGN UP
+        </v-btn>
+    </v-toolbar-items>
+
   </v-toolbar>
 </template>
 
 <script>
+
 export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
 }
 
 </script>
+
+<style scoped>
+  .home {
+    cursor: pointer;
+  }
+  .home:hover {
+    color: brown;
+  }
+</style>
