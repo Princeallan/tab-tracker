d5161e447ec8102ae1e5e612668561e3d351dc8a
princeallan <allankirui57@gmail.com>
2018-12-20 22:02:01 +0300

    Login Backend routes & controller

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -10,5 +10,37 @@ module.exports = {
         error: 'Email already registered to a different user'
       })
     }
+  },
+  async login (req, res) {
+    try {
+      const { email, password } = req.body
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      })
+
+      if (!user) {
+        return res.status(403).send({
+          error: 'The login information is incorrect1'
+        })
+      }
+
+      const isPasswordValid = password === user.password
+      if (!isPasswordValid) {
+        return res.status(403).send({
+          error: 'The login information is incorrect2'
+        })
+      }
+
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson
+      })
+    } catch (err) {
+      res.status(500).send({
+        error: 'Error occurred while trying to login'
+      })
+    }
   }
 }
